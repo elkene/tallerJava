f7f44341e9eaed744e3ff546371e43bf8ce6588f
elkene <kenneth.armenta@uabc.edu.mx>
2024-08-26 11:51:26 -0700

    actualizado

--- src/figuras/AlmacenDeFiguras.java
+++ src/figuras/AlmacenDeFiguras.java
@@ -1,23 +1,52 @@
 package figuras;
 
 public class AlmacenDeFiguras {
+    //Cuadrado
     static Cuadrado[] miscuadrados;
+    static int indiceCuadrado;
+
+    //Circulo
     static Circulo[] miscirculos;
+    static int indiceCirculo;
+
+    //Triangulo
     static Triangulo[] mistriangulos;
-    static int indiceCuadrado;  
+    static int indiceTriangulo;
 
     public static void inicializa() {
+        //Cuadrado
         miscuadrados = new Cuadrado[5];
-        miscirculos = new Circulo[5];  // Agregar inicialización de los otros arrays si se necesitan
-        mistriangulos = new Triangulo[5];
         indiceCuadrado = 0;
+        //Circulo
+        miscirculos = new Circulo[5];
+        //Triangulo  
+        mistriangulos = new Triangulo[5];
     }
 
-    public static String insertarCuadrado(Cuadrado c) {
+    public static String insertarCuadrado(Cuadrado cuadrado) {
         if (indiceCuadrado < miscuadrados.length) {
-            miscuadrados[indiceCuadrado] = c;
+            miscuadrados[indiceCuadrado] = cuadrado;
             indiceCuadrado++;
             return "Cuadrado insertado correctamente";
+        } else {
+            return "Almacen lleno";
+        }
+    }
+    public static String insertarCirculo(Circulo circulo) {
+        if (indiceCirculo < miscirculos.length) {
+            miscirculos[indiceCirculo] = circulo;
+            indiceCirculo++;
+            return "Circulo insertado correctamente";
+        } else {
+            return "Almacen lleno";
+        }
+    }
+
+    public static String insertarTriangulo(Triangulo triangulo) {
+        if (indiceTriangulo < mistriangulos.length) {
+            mistriangulos[indiceTriangulo] = triangulo;
+            indiceTriangulo++;
+            return "Circulo insertado correctamente";
         } else {
             return "Almacen lleno";
         }
